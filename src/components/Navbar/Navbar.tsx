49af664d9127768fba7f85d17a730be08305c72d
Maria del Mar <chocoinformatics2016@gmail.com>
2024-04-05 01:30:21 -0400

    everything until server configuration

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,8 +3,12 @@ import MaxWidthWrapper from "../MaxWidthWrapper";
 import Link from "next/link";
 import { Icons } from "../Icons/Icons";
 import NavItems from "./NavItems";
+import { buttonVariants } from "../ui/button";
+import Cart from "../Cart/Cart";
 
 const Navbar = () => {
+  const user = null;
+
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
       <header className="relative bg-white">
@@ -16,11 +20,49 @@ const Navbar = () => {
               <div className="ml-4 flex lg:ml-0">
                 {/* MAKE THE LOGO */}
                 <Link href="/">
-                  <Icons.logo className="h-10 w-10" />
+                  <Icons.logo className="h-10 w-40" />
                 </Link>
               </div>
               <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
                 <NavItems />
+              </div>
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  {!user && (
+                    <Link
+                      href="/sign-in"
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Sign in
+                    </Link>
+                  )}
+                  {!user && (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden />
+                  )}
+
+                  {!user && (
+                    <Link
+                      href="/sign-up"
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Create Account
+                    </Link>
+                  )}
+
+                  {user && (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden />
+                  )}
+
+                  {!user && (
+                    <div className="flex lg:ml-6">
+                      <span className="h-6 w-px bg-gray-200" aria-hidden />
+                    </div>
+                  )}
+
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Cart />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
